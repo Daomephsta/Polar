d5a793b641ae0e24d5f6e6b53e550bc1e4d01b33
Daomephsta <dtnilfisk@gmail.com>
2018-04-11 13:17:10 +1200

    Notify proxy of init stages & correct server proxy path

--- src/main/java/leviathan143/polar/common/Polar.java
+++ src/main/java/leviathan143/polar/common/Polar.java
@@ -5,7 +5,7 @@ import leviathan143.polar.common.capabilities.CapabilityPlayerDataPolar;
 import leviathan143.polar.common.core.InternalMethodAccessors;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.*;
 
 
 @Mod(modid = Polar.MODID, name = Polar.MODNAME, version = Polar.VERSION, dependencies = Polar.DEPENDENCIES)
@@ -16,15 +16,28 @@ public class Polar
 	public static final String VERSION = "0.0.1";
 	public static final String DEPENDENCIES = "required-before:guideapi";
 	public static final String CLIENT_PROXY_PATH = "leviathan143.polar.client.ClientProxy";
-	public static final String SERVER_PROXY_PATH = "leviathan143.polar.common.ServerProxy";
+	public static final String SERVER_PROXY_PATH = "leviathan143.polar.server.ServerProxy";
 	
 	@SidedProxy(serverSide=Polar.SERVER_PROXY_PATH, clientSide=Polar.CLIENT_PROXY_PATH)
 	public static CommonProxy proxy;
 	
 	@Mod.EventHandler
-	public static void preinit(FMLPreInitializationEvent event)
+	public static void preInit(FMLPreInitializationEvent event)
 	{
+		proxy.preInit(event);
 		PolarAPI.initialiseAPI(new InternalMethodAccessors());
 		CapabilityPlayerDataPolar.register();
+	}
+	
+	@Mod.EventHandler
+	public static void init(FMLInitializationEvent event)
+	{
+		proxy.init(event);
+	}
+	
+	@Mod.EventHandler
+	public static void postInit(FMLPostInitializationEvent event)
+	{
+		proxy.postInit(event);
 	}
 }
