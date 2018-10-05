402a5e60a058f2e6b168c9ef83209515a5faf2ac
Daomephsta <dtnilfisk@gmail.com>
2018-10-05 22:22:31 +1300

    Update basics category image and anomalies entry image

--- polar-api/src/main/java/leviathan143/polar/api/guide/PolarCategories.java
+++ polar-api/src/main/java/leviathan143/polar/api/guide/PolarCategories.java
@@ -2,10 +2,12 @@ package leviathan143.polar.api.guide;
 
 import amerifrance.guideapi.api.impl.abstraction.CategoryAbstract;
 import amerifrance.guideapi.category.CategoryItemStack;
+import amerifrance.guideapi.category.CategoryResourceLocation;
 import leviathan143.polar.api.PolarAPI;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 
 /**
  * Provides access to Polar's guide categories.
@@ -13,7 +15,7 @@ import net.minecraft.item.ItemStack;
  */
 public class PolarCategories
 {
-	public static final CategoryAbstract BASICS = new CategoryItemStack(createCategoryName("basics"), new ItemStack(Blocks.LOG));
+	public static final CategoryAbstract BASICS = new CategoryResourceLocation(createCategoryName("basics"), new ResourceLocation(PolarAPI.PROVIDER_MOD_ID, "textures/guide/misc/anomalies.png"));
 	public static final CategoryAbstract COMBAT = new CategoryItemStack(createCategoryName("combat"), new ItemStack(Items.IRON_SWORD));
 	public static final CategoryAbstract FARMING = new CategoryItemStack(createCategoryName("farming"), new ItemStack(Items.IRON_HOE));
 	public static final CategoryAbstract BUILDING = new CategoryItemStack(createCategoryName("building"), new ItemStack(Blocks.BRICK_BLOCK));
