beab0de4710d032ed69baba4e456b3d7e324ab1e
Daomephsta <dtnilfisk@gmail.com>
2022-11-24 11:18:43 +1300

    Improve tag helper method names

--- src/main/java/io/github/daomephsta/polar/common/CompatibilityTags.java
+++ src/main/java/io/github/daomephsta/polar/common/CompatibilityTags.java
@@ -5,30 +5,36 @@ import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 
 public class CompatibilityTags
 {
-    public static final TagKey<Item> MELEE_WEAPONS = registerItemTag("melee_weapons"),
-                                  SWORDS = registerItemTag("swords"),
-                                  AXES = registerItemTag("axes"),
-                                  HEAD_EQUIPMENT = registerItemTag("head_equipment"),
-                                  CHEST_EQUIPMENT = registerItemTag("chest_equipment"),
-                                  LEGS_EQUIPMENT = registerItemTag("legs_equipment"),
-                                  FEET_EQUIPMENT = registerItemTag("feet_equipment");
-    public static final TagKey<Block> GRAVITY_AFFECTED = registerBlockTag("gravity_affected");
+    public static final TagKey<Item> MELEE_WEAPONS = itemTagKey("melee_weapons"),
+                                  SWORDS = itemTagKey("swords"),
+                                  AXES = itemTagKey("axes"),
+                                  HEAD_EQUIPMENT = itemTagKey("head_equipment"),
+                                  CHEST_EQUIPMENT = itemTagKey("chest_equipment"),
+                                  LEGS_EQUIPMENT = itemTagKey("legs_equipment"),
+                                  FEET_EQUIPMENT = itemTagKey("feet_equipment");
+    public static final TagKey<Block> GRAVITY_AFFECTED = blockTagKey("gravity_affected");
 
     public static void initialise()
     {
         //Dummy method to force static init
     }
 
-    private static TagKey<Item> registerItemTag(String name)
+    private static TagKey<Item> itemTagKey(String name)
     {
-        return TagKey.of(Registry.ITEM_KEY, new Identifier("c", name));
+        return tagKey(Registry.ITEM_KEY, name);
     }
 
-    private static TagKey<Block> registerBlockTag(String name)
+    private static TagKey<Block> blockTagKey(String name)
     {
-        return TagKey.of(Registry.BLOCK_KEY, new Identifier("c", name));
+        return tagKey(Registry.BLOCK_KEY, name);
+    }
+
+    private static <T> TagKey<T> tagKey(RegistryKey<Registry<T>> registry, String name)
+    {
+        return TagKey.of(registry, new Identifier("c", name));
     }
 }
--- src/main/java/io/github/daomephsta/polar/common/PolarTags.java
+++ src/main/java/io/github/daomephsta/polar/common/PolarTags.java
@@ -4,6 +4,7 @@ import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryKey;
 
 public class PolarTags
 {
@@ -13,14 +14,19 @@ public class PolarTags
     }
 
     @SuppressWarnings("unused")
-    private static TagKey<Item> registerItemTag(String name)
+    private static TagKey<Item> itemTagKey(String name)
     {
-        return TagKey.of(Registry.ITEM_KEY, Polar.id(name));
+        return tagKey(Registry.ITEM_KEY, name);
     }
 
     @SuppressWarnings("unused")
-    private static TagKey<Block> registerBlockTag(String name)
+    private static TagKey<Block> blockTagKey(String name)
     {
-        return TagKey.of(Registry.BLOCK_KEY, Polar.id("name"));
+        return tagKey(Registry.BLOCK_KEY, name);
+    }
+
+    private static <T> TagKey<T> tagKey(RegistryKey<Registry<T>> registry, String name)
+    {
+        return TagKey.of(registry, Polar.id(name));
     }
 }
